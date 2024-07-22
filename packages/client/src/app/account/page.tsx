de3a456c96565ac485eea876d5995d6a988876e8
winetree94 <winetree94@gmail.com>
2024-07-22 16:00:04 +0900

    fix: build problems

--- packages/client/src/app/account/page.tsx
+++ packages/client/src/app/account/page.tsx
@@ -1,11 +1,11 @@
 import { UserList } from "@/components/UserList";
-import { trpc } from "@/utils/trpc";
+// import { trpc } from "@/utils/trpc";
 
 export default async function Account() {
-  const data = await trpc.account.list.query();
+  // const data = await trpc.account.list.query();
   return (
     <main>
-      {JSON.stringify(data)}
+      {/* {JSON.stringify(data)} */}
       <UserList></UserList>
       <div>Account</div>
     </main>
